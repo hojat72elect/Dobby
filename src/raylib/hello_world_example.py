d87f9bdca0b251ecb56af0864e949a7a91717df2
hojat <hojat72elect@gmail.com>
2025-02-16 21:05:13 -0500

    hello world example of raylib.

---
+++ src/raylib/hello_world_example.py
@@ -0,0 +1,13 @@
+import pyray
+
+
+if __name__ == '__main__':
+
+    pyray.init_window(800, 450, "Hello World Example")
+    while not pyray.window_should_close():
+        pyray.begin_drawing()
+        pyray.clear_background(pyray.WHITE)
+        pyray.draw_text("Hello World\nMy name is RayLib.", 190, 200, 20, pyray.VIOLET)
+        pyray.end_drawing()
+
+    pyray.close_window()
