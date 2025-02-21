06ded8ee2da948c2d2df9dd58d99a07d3b25bc2c
hojat <hojat72elect@gmail.com>
2025-02-21 02:13:02 -0500

    cleaned a bit

--- src/oop/higher_or_lower.py
+++ src/oop/higher_or_lower.py
@@ -19,21 +19,19 @@ def shuffle(deck: list[str]) -> list[str]:
 
 if __name__ == '__main__':
     # Card constants
-    SUIT_TUPLE = ('Spades', 'Hearts', 'Clubs', 'Diamonds')
-    RANK_TUPLE = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
+    SUITS = ('Spades', 'Hearts', 'Clubs', 'Diamonds')
+    RANKS = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
 
-    NCARDS = 8
+    NUMBER_CARDS = 8
 
-    #  Main code
     print('Welcome to Higher or Lower.')
     print('You have to choose whether the next card to be shown will be higher or lower than the current card.')
     print('Getting it right adds 20 points; get it wrong and you lose 15 points.')
-    print('You have 50 points to start.')
-    print()
+    print('You have 50 points to start.\n')
 
     startingDeckList = []
-    for suit in SUIT_TUPLE:
-        for thisValue, rank in enumerate(RANK_TUPLE):
+    for suit in SUITS:
+        for thisValue, rank in enumerate(RANKS):
             cardDict = {'rank': rank, 'suit': suit, 'value': thisValue + 1}
             startingDeckList.append(cardDict)
 
@@ -49,7 +47,7 @@ if __name__ == '__main__':
         print('Starting card is:', currentCardRank + ' of ' + currentCardSuit)
         print()
 
-        for cardNumber in range(0, NCARDS):  # play one game of this many cards
+        for cardNumber in range(0, NUMBER_CARDS):  # play one game of this many cards
             answer = input('Will the next card be higher or lower than the ' +
                            currentCardRank + ' of ' +
                            currentCardSuit + '?  (enter h or l): ')
