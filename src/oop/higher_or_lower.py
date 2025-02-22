8205291bf2b2fca35c12f9029c41326739e584d2
hojat <hojat72elect@gmail.com>
2025-02-21 23:09:37 -0500

    Cleaned and added some extra typing.

--- src/oop/higher_or_lower.py
+++ src/oop/higher_or_lower.py
@@ -1,14 +1,14 @@
 import random
 
 
-def getCard(deck: list[str]) -> str:
+def getCard(deck: list[any]) -> any:
     """
     You give this function a deck of cards, and it returns the first one for you.
     """
     return deck.pop()
 
 
-def shuffle(deck: list[str]) -> list[str]:
+def shuffle(deck: list[any]) -> list[any]:
     """
     You give this function a deck of cards, and it returns a shuffled copy of it.
     """
@@ -18,7 +18,7 @@ def shuffle(deck: list[str]) -> list[str]:
 
 
 if __name__ == '__main__':
-    # Card constants
+
     SUITS = ('Spades', 'Hearts', 'Clubs', 'Diamonds')
     RANKS = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
 
@@ -29,57 +29,56 @@ if __name__ == '__main__':
     print('Getting it right adds 20 points; get it wrong and you lose 15 points.')
     print('You have 50 points to start.\n')
 
-    startingDeckList = []
-    for suit in SUITS:
-        for thisValue, rank in enumerate(RANKS):
-            cardDict = {'rank': rank, 'suit': suit, 'value': thisValue + 1}
-            startingDeckList.append(cardDict)
+    all_cards: list[dict[str, str | int]] = []
+    for card_suit in SUITS:
+        for card_value, card_rank in enumerate(RANKS):
+            _card: dict[str, str | int] = {'rank': card_rank, 'suit': card_suit, 'value': card_value + 1}
+            all_cards.append(_card)
 
-    score = 50
+    player_score: int = 50
 
-    while True:  # play multiple games
+    while True:
         print()
-        gameDeckList = shuffle(startingDeckList)
-        currentCardDict = getCard(gameDeckList)
-        currentCardRank = currentCardDict['rank']
-        currentCardValue = currentCardDict['value']
-        currentCardSuit = currentCardDict['suit']
-        print('Starting card is:', currentCardRank + ' of ' + currentCardSuit)
-        print()
+        game_deck: list[dict[str, str | int]] = shuffle(all_cards)
+        current_card: dict[str, str | int] = getCard(game_deck)
+        current_card_rank: str = current_card['rank']
+        current_card_value: int = current_card['value']
+        current_card_suit: str = current_card['suit']
+        print(f'Starting card is : {current_card_rank} of {current_card_suit}\n')
 
         for cardNumber in range(0, NUMBER_CARDS):  # play one game of this many cards
             answer = input('Will the next card be higher or lower than the ' +
-                           currentCardRank + ' of ' +
-                           currentCardSuit + '?  (enter h or l): ')
+                           current_card_rank + ' of ' +
+                           current_card_suit + '?  (enter h or l): ')
             answer = answer.casefold()  # force lower case
-            nextCardDict = getCard(gameDeckList)
+            nextCardDict = getCard(game_deck)
             nextCardRank = nextCardDict['rank']
             nextCardSuit = nextCardDict['suit']
             nextCardValue = nextCardDict['value']
             print('Next card is:', nextCardRank + ' of ' + nextCardSuit)
 
             if answer == 'h':
-                if nextCardValue > currentCardValue:
+                if nextCardValue > current_card_value:
                     print('You got it right, it was higher')
-                    score = score + 20
+                    player_score = player_score + 20
                 else:
                     print('Sorry, it was not higher')
-                    score = score - 15
+                    player_score = player_score - 15
 
             elif answer == 'l':
-                if nextCardValue < currentCardValue:
-                    score = score + 20
+                if nextCardValue < current_card_value:
+                    player_score = player_score + 20
                     print('You got it right, it was lower')
 
                 else:
-                    score = score - 15
+                    player_score = player_score - 15
                     print('Sorry, it was not lower')
 
-            print('Your score is:', score)
+            print('Your score is:', player_score)
             print()
-            currentCardRank = nextCardRank
-            currentCardValue = nextCardValue
-            currentCardSuit = nextCardSuit
+            current_card_rank = nextCardRank
+            current_card_value = nextCardValue
+            current_card_suit = nextCardSuit
 
         goAgain = input('To play again, press ENTER, or "q" to quit: ')
         if goAgain == 'q':
