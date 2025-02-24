522bbbf61ea00d7d7ad16164c3ad1ef84822016d
hojat <hojat72elect@gmail.com>
2025-02-24 03:09:31 -0500

    world_grid_example.py : improved imports

--- src/ursina/world_grid_example.py
+++ src/ursina/world_grid_example.py
@@ -1,18 +1,18 @@
-from ursina import Ursina, Entity, color, Grid, duplicate, EditorCamera, lerp
+import ursina
 
 if __name__ == '__main__':
-    # The easiest ursina game you could make
-    app = Ursina()
+
+    app = ursina.Ursina()
 
     r = 8
     for i in range(1, r):
         t = i / r
         s = 4 * i
         print(s)
-        grid = Entity(model=Grid(s, s), scale=s, color=color.hsv(0, 0, .8, lerp(.8, 0, t)), rotation_x=90, y=i / 1000)
-        subgrid = duplicate(grid)
-        subgrid.model = Grid(s * 4, s * 4)
-        subgrid.color = color.hsv(0, 0, .4, lerp(.8, 0, t))
-        EditorCamera()
+        grid = ursina.Entity(model=ursina.Grid(s, s), scale=s, color=ursina.color.hsv(0, 0, .8, ursina.lerp(.8, 0, t)), rotation_x=90, y=i / 1000)
+        subgrid = ursina.duplicate(grid)
+        subgrid.model = ursina.Grid(s * 4, s * 4)
+        subgrid.color = ursina.color.hsv(0, 0, .4, ursina.lerp(.8, 0, t))
+        ursina.EditorCamera()
 
     app.run()
