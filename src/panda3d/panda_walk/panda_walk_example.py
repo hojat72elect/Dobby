c7b5fcaf199b09dc2ffbdea4e8c11502d1b53621
hojat <hojat72elect@gmail.com>
2025-02-27 17:41:28 -0500

    Added docs

--- src/panda3d/panda_walk/panda_walk_example.py
+++ src/panda3d/panda_walk/panda_walk_example.py
@@ -5,37 +5,35 @@ from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3
+from panda3d.core import NodePath
 
-"""
-Shows a panda walking in a glade. And the camera spins around it.
-"""
+
 class PandaWalkExample(ShowBase):
+    """
+    Shows a panda walking in a glade. And the camera spins around it.
+    """
+
     def __init__(self):
         ShowBase.__init__(self)
 
-        # Disable the camera trackball controls.
-        self.disableMouse()
+        self.disableMouse()  # Disable mouse control over camera
 
-        # Load the environment model.
-        self.scene = self.loader.loadModel("models/environment")
-        # Reparent the model to render.
+        self.scene: NodePath = self.loader.loadModel("models/environment")  # An internal model of the engine
         self.scene.reparentTo(self.render)
-        # Apply scale and position transforms on the model.
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
 
         # Add the spinCameraTask procedure to the task manager (so the camera will be spinning).
-        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+        self.taskMgr.add(self.spinCamera, "SpinCameraTask")
 
-        # Load and transform the panda actor.
+        # Load the Panda model and attach the "walk" animation to it.
         self.pandaActor = Actor("models/panda-model", {"walk": "models/panda-walk4"})
         self.pandaActor.setScale(0.005, 0.005, 0.005)
         self.pandaActor.reparentTo(self.render)
-        # Loop its animation.
+
         self.pandaActor.loop("walk")
 
-        # Create the four lerp intervals needed for the panda to
-        # walk back and forth.
+        # Create the four lerp intervals needed for the panda to walk back and forth.
         posInterval1 = self.pandaActor.posInterval(13, Point3(0, -10, 0), startPos=Point3(0, 10, 0))
         posInterval2 = self.pandaActor.posInterval(13, Point3(0, 10, 0), startPos=Point3(0, -10, 0))
         hprInterval1 = self.pandaActor.hprInterval(3, Point3(180, 0, 0), startHpr=Point3(0, 0, 0))
@@ -45,7 +43,7 @@ class PandaWalkExample(ShowBase):
         self.pandaPace = Sequence(posInterval1, hprInterval1, posInterval2, hprInterval2, name="pandaPace")
         self.pandaPace.loop()
 
-    def spinCameraTask(self, task) -> int:
+    def spinCamera(self, task) -> int:
         angleDegrees = task.time * 6.0
         angleRadians = angleDegrees * (pi / 180.0)
         self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
@@ -54,5 +52,4 @@ class PandaWalkExample(ShowBase):
 
 
 if __name__ == '__main__':
-    app = PandaWalkExample()
-    app.run()
+    PandaWalkExample().run()
