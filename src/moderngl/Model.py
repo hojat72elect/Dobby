e8e9e55a303f7e30219b65d670f7be207c2e595a
hojat <hojat72elect@gmail.com>
2025-02-28 00:41:11 -0500

    worked a bit on the example from moderngl (will do the rest of it later)

--- src/moderngl/Model.py
+++ src/moderngl/Model.py
@@ -1,4 +1,16 @@
+import numpy
+
 class Triangle:
     def __init__(self, app):
         self.app = app
         self.opengl_context = app.opengl_context
+
+    def get_vertex_data(self):
+        vertex_data = [(-0.6, -0.8, 0.0), (0.6, -0.8, 0.0), (0.0, 0.8, 0.0)]
+        vertex_data = numpy.array(vertex_data, dtype='f4')
+        return vertex_data
+
+    def get_vbo(self):
+        vertex_data = self.get_vertex_data()
+        vbo = self.opengl_context.buffer(vertex_data)
+        return vbo
